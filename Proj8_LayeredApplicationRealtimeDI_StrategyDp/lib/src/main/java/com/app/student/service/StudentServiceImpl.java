6f705a888c04ca7d7a37fa31689288b59edb849f
ankitaryasam <ankitrajaci@gmail.com>
2024-07-08 23:57:21 +0530

    spring proj 8

---
+++ Proj8_LayeredApplicationRealtimeDI_StrategyDp/lib/src/main/java/com/app/student/service/StudentServiceImpl.java
@@ -0,0 +1,74 @@
+package com.app.student.service;
+
+import com.app.student.dao.StudentDao;
+import com.app.student.dto.StudentDto;
+import com.app.student.entity.StudentEntity;
+
+public final class StudentServiceImpl implements StudentService {
+	
+	private StudentDao studentDao;
+	
+	
+
+	public StudentServiceImpl(StudentDao studentDao) {
+		this.studentDao = studentDao;
+	}
+
+
+
+	@Override
+	public int registerNewStudent(StudentDto dto) throws Exception {
+		StudentEntity entity = null;
+		String grade = null;
+		
+		//converting dto to entity
+		entity = new StudentEntity();
+		entity.setId(dto.getId());
+		entity.setName(dto.getName());
+		entity.setEmail(dto.getEmail());
+		entity.setCourseName(dto.getCourseName());
+		entity.setObtMarks(dto.getObtMarks());
+		
+		//calculate gradle
+		if(dto.getObtMarks() >= 400) {
+			grade = "A";
+		}
+		else if(dto.getObtMarks() >= 350) {
+			grade="B";
+		}
+		else if(dto.getObtMarks() >= 300) {
+		    grade="C";
+		}
+		else if(dto.getObtMarks() >= 200) {
+			grade="D";
+		}
+		else {
+			grade="Fail";
+		}
+		
+		dto.setGrade(grade);
+		entity.setGrade(dto.getGrade());
+		int i = studentDao.saveStudent(entity);
+		return i;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}

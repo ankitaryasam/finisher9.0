011e9e9eac0c166e61f9aded694b4a8878c5c787
ankitaryasam <ankitrajaci@gmail.com>
2024-09-12 23:35:48 +0530

    spring proj 55 + 56

---
+++ Proj56_AspecjAOP_JDKLIB_ProxyClass/lib/src/main/java/com/app/service/BankService.java
@@ -0,0 +1,9 @@
+package com.app.service;
+
+//as proxy interface...
+public interface BankService {
+	
+	public float calSi(float p, float r, float t);
+	public float calCi(float p, float r, float t);
+
+}
---
+++ Proj56_AspecjAOP_JDKLIB_ProxyClass/lib/src/main/java/com/app/service/BankServiceImpl.java
@@ -0,0 +1,17 @@
+package com.app.service;
+
+public final class BankServiceImpl implements BankService {
+
+	public  float  calSi(float p, float r, float t) {
+		System.out.println("BankService.calSi()");
+		return  p*t*r / 100.0f;
+	}
+	
+	
+	public float  calCi(float p, float r, float t) {
+		System.out.println("BankService.calCi()");
+		float ci = (float)((p * Math.pow(1+r/100, t)))-p;
+		return ci;
+	}
+
+}

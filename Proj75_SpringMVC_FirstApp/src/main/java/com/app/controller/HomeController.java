f85b2516607a49d2fee1874bc868c57ed4da684a
ankitaryasam <ankitrajaci@gmail.com>
2024-11-05 00:55:15 +0530

    spring proj 75

---
+++ Proj75_SpringMVC_FirstApp/src/main/java/com/app/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.app.controller;
+
+import java.util.Date;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HomeController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		Date d = new Date();
+		ModelAndView mav = new ModelAndView("home", "currentDate", d);
+		return mav;
+	}
+
+}
---
+++ Proj75_SpringMVC_FirstApp/src/main/webapp/WEB-INF/pages/home.jsp
@@ -0,0 +1,8 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+
+<h1 style="color:red; text-align:center;">Welcome to my first spring mvc project</h1>
+
+<p>OUTPUT :: </p>
+
+
+<p>Date and Time : ${currentDate}</p>
